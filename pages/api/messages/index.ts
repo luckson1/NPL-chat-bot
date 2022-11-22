8190af7413074bf5d38626cab89a02844781cbe4
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-11-22 17:44:02 +0300

    fix: connecting to DB

--- pages/api/messages/index.ts
+++ pages/api/messages/index.ts
@@ -1,13 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "../config/dbConfig";
 import { createMessage, getMessages } from "../controllers/messages";
 
 export default async function hundler(req: NextApiRequest, res: NextApiResponse) {
 const {method}=req
 switch (method) {
     case "GET":
+        await dbConnect()
         getMessages(req, res)
         break;
     case "POST": 
+    await dbConnect()
     createMessage(req, res)
     break;
 }
